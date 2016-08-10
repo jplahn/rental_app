451e5458ad7cdf4c1557c5d8aa22380bfb33fb46
Jordan Plahn <jplahn@vt.edu>
2016-08-09 22:38:31 -0700

    Add first level of form input

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def get_index():
-    return 'Hello, World!'
+    if request.method == 'POST':
+        city = request['city']
+        return render_template('city.html', city=city)
+    else:
+        return render_template('index.html')
