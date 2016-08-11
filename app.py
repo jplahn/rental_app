91243f9bfbd4772bd6051cccfb2c004f364bf0a1
Jordan Plahn <jplahn@vt.edu>
2016-08-10 22:26:31 -0700

    Add PyMongo integration

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, request, render_template, redirect, url_for
 from forms import CityForm
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
+
+# configure MongoDB values
+app.config['MONGO_HOST'] = 'ds153715.mlab.com'
+app.config['MONGO_PORT'] = '53715'
+app.config['MONGO_DBNAME'] = 'rental_app'
+app.config['MONGO_USERNAME'] = 'test'
+app.config['MONGO_PASSWORD'] = 'test'
+mongo = PyMongo(app, config_prefix='MONGO')
 
 @app.route('/', methods=['POST', 'GET'])
 def get_index():
@@ -11,3 +20,9 @@ def get_index():
         return render_template('city.html', city=city)
     else:
         return render_template('index.html')
+
+
+@app.route('/db')
+def get_data():
+    result = mongo.db.monthly_rent_average.find_one_or_404({'city': 'Seattle'})
+    print result
