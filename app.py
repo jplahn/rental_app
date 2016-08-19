ed4661986505f825855d4618899129cccbbd103c
nitishdhar <dhar.nitish@gmail.com>
2016-08-18 23:08:44 -0700

    Flash, Validation & UI
    Flash Messages with categories, Form post validation, UI Improvements

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, flash, request, render_template, redirect, url_for, send_from_directory
+from flask import Flask, flash, request, render_template, redirect, url_for
 from forms import CityForm
 from flask.ext.pymongo import PyMongo
 
@@ -17,20 +17,24 @@ def send_assets(resource, path):
 @app.route('/', methods=['POST', 'GET'])
 def get_index():
     form = CityForm(csrf_enabled=False)
-    if form.validate_on_submit():
-        city = form.city.data
-        return redirect(url_for('get_data', city=city))
-    else:
-        return render_template('index.html')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            city = form.city.data
+            return redirect(url_for('get_data', city=city))
+        else:
+            flash('Please fill in the required details', 'danger')
+
+    return render_template('index.html', form=form)
 
 
 @app.route('/<city>')
 def get_data(city):
     result = mongo.db.monthly_rent_average.find_one({'city': city})
     if result:
+        flash('Alright alright alright, city ' + city + ' found!', 'success')
         return render_template('city.html', city=city)
     else:
-        flash('Oops! City doesn\'t exist in the DB yet!')
+        flash('Oops! City ' + city + ' doesn\'t exist in our DB yet!', 'danger')
         return render_template('index.html')
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import Form
 from wtforms import StringField, fields
+from wtforms.validators import DataRequired
 
 class CityForm(Form):
-    city = StringField('city')
+    city = StringField('city', validators=[DataRequired()])
